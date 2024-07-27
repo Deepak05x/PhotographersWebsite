899c6d2cd6be644f618d4e84b1919626c1379cbb
Deepak <hdeepak5902@gmail.com>
2024-07-27 22:57:06 +0530

    Changes Done

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -19,7 +19,7 @@ const ContactPage = () => {
                         <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
                             <span className="w-[30px] h-[2px] bg-primary"></span>Hello
                         </div>
-                        <h1 className="h1 max-w-md mb-8">Let's Work Togeather</h1>
+                        <h1 className="h1 max-w-md mb-8">Let&apos;s Work Togeather</h1>
                         <p className="subtitle max-w-[400px]">
                             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique vero odit assumenda voluptate minima quam! Commodi aliquam, in cum placeat sit veniam soluta asperiores
                             vitae, aspernatur aliquid quam earum molestias!
