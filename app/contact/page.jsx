9766793fb003cd563ecb5834f0293236fbc03b61
Deepak <hdeepak5902@gmail.com>
2024-07-27 22:52:06 +0530

    Finalized

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,12 +1,21 @@
+"use client";
+
 import React from "react";
 import Form from "@/components/Form";
+import { motion } from "framer-motion";
 
 const ContactPage = () => {
     return (
         <section className="min-h-screen mt-24">
             <div className="container mx-auto">
                 <div className="grid xl:grid-cols-2 pt-12 mb-6 xl:mb-24">
-                    <div className="flex flex-col justify-center">
+                    <motion.div
+                        initial={{ opacity: 0, x: 150 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ delay: 0.3, duration: 0.8, ease: "easeInOut" }}
+                        viewport={{ once: true }}
+                        className="flex flex-col justify-center"
+                    >
                         <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
                             <span className="w-[30px] h-[2px] bg-primary"></span>Hello
                         </div>
@@ -15,10 +24,16 @@ const ContactPage = () => {
                             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique vero odit assumenda voluptate minima quam! Commodi aliquam, in cum placeat sit veniam soluta asperiores
                             vitae, aspernatur aliquid quam earum molestias!
                         </p>
-                    </div>
-                    <div className="lg:mt-24">
+                    </motion.div>
+                    <motion.div
+                        initial={{ opacity: 0, x: -150 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ delay: 0.3, duration: 0.8, ease: "easeInOut" }}
+                        viewport={{ once: true }}
+                        className="lg:mt-24"
+                    >
                         <Form />
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </section>
--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -12,9 +12,9 @@ const MobileNav = () => {
             </SheetTrigger>
             <SheetContent>
                 <div className="flex flex-col items-center justify-between h-full py-8">
-                    <div className="flex flex-col gap-8 pt-12">
+                    <div className="flex items-center flex-col gap-16 pt-24">
                         <Logo />
-                        <Navbar containerStyles="flex flex-col items-center gap-6" linkStyles="text-1xl" />
+                        <Navbar containerStyles="flex flex-col items-center text-lg gap-6" linkStyles="text-1xl" />
                     </div>
                     <Socials containerStyles="flex gap-4" iconStyles="text-2xl" />
                 </div>
